d60fe06f9a52fbb0db4dc562eeec2e4162468622
Rizalstg <rizalsitanggang39@gmail.com>
2023-12-09 20:40:59 +0700

    push to github

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JobSheet1dan2"
+include(":app")
